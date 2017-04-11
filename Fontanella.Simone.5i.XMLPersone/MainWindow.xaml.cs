b3d92388649de1a1d355865b99a972041fea41b9
simonefontanella <fonta9879@gmail.com>
2017-04-11 10:07:17 +0200

    Aggiunto Progetto

---
+++ Fontanella.Simone.5i.XMLPersone/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Fontanella.Simone._5i.XMLPersone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                XElement dati = XElement.Load("Persone.xml");
+                dati.Element("metei").Add(
+                            new XElement("Meteo",
+                            new XAttribute("Citta", "Prova"),
+                            new XAttribute("Temperatura", "123")
+                    );
+                dgDati.ItemsSource = from l in dati.Elements("Persone").First().Elements("Persona")
+                                     select new Persona(l);
+            }
+            catch (Exception errore)
+            {
+                MessageBox.Show(errore.Message);
+            }
+        }
+    }
+}
---
+++ Fontanella.Simone.5i.XMLPersone/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Fontanella.Simone._5i.XMLPersone
+{
+    class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Numero { get; set; }
+        public string Indirizzo { get; set; }
+
+        public Persona(XElement e)
+        {
+            Nome = e.Attribute("Nome").Value;
+            Cognome = e.Attribute("Cognome").Value;
+            Numero = e.Attribute("Numero").Value;
+            Indirizzo = e.Attribute("Indirizzo").Value;
+        }
+    }
+}
